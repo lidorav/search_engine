6b300bfce393646a390910ce4c59c22ee6495409
Nir Kutsky <nirk2012@gmail.com>
2018-11-10 18:38:52 +0200

    Tokens class added

--- src/main/java/Model/Parse/Tokens.java
+++ src/main/java/Model/Parse/Tokens.java
@@ -2,6 +2,7 @@ package Model.Parse;
 
 import Model.Term;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
@@ -9,6 +10,18 @@ public class Tokens {
 
     private List<String> tokenList;
     private HashMap<String, Term> terms = new HashMap<>();
+
+    public String getTokenFromList(int index){
+        return tokenList.get(index);
+    }
+
+    public void saveTerm(String termName, Term term){
+        terms.put(termName, term);
+    }
+
+    public Collection<Term> getTerms(){
+        return terms.values();
+    }
 
 
 
