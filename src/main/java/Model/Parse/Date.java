27c7980d70e326e49e209b54b0e9b0cfefe51cb1
lidorav <38429030+lidorav@users.noreply.github.com>
2018-11-12 18:53:19 +0200

    Merge branch 'master' of https://github.com/lidorav/search_engine
    # Conflicts:
    #	src/main/java/Main.java

--- src/main/java/Model/Parse/Date.java
+++ src/main/java/Model/Parse/Date.java
@@ -1,26 +1,51 @@
 package Model.Parse;
 
 
+import java.lang.reflect.Array;
 import java.time.Month;
 
 public class Date {
 
-    private String[]  dateArr = {"January" ,"February" ,"March" ,"April","May","June","July","August" };
-
-
-
-
+    private static final String[]  dateArr = {"january" ,"jan" ,"february" ,"feb" ,"march" ,"mar" ,"april" ,"apr" ,
+            "may" ,"june" ,"jun" ,"july" ,"jul" ,"august" ,"aug" ,"september" ,"sep" ,
+            "october" ,"oct" ,"november" ,"nov" ,"december" ,"dec"};
 
     public String datePars (int index , String str) {
-        String nextToken = Parser.getTokenFromList(index+1);;
+        String nextToken = Parser.getTokenFromList(index + 1);
 
-        if(str.chars().allMatch(Character::isDigit)){
-
+        //DD Month pattern
+        if (str.chars().allMatch(Character::isDigit) && isMonth(nextToken)) {
+            Parser.index++;
+            return Month.valueOf(nextToken.toUpperCase()).getValue() + "-" + str;
+        }
+        //Month DD pattern
+        if(isMonth(str) && !isYear(nextToken)){
+            Parser.index++;
+            return Month.valueOf(str.toUpperCase()).getValue() + "-" + nextToken;
+        }
+        //YYYY MM pattern
+        if(isMonth(str) && isYear((nextToken))){
+            Parser.index++;
+            return Month.valueOf(str.toUpperCase()).getValue() + "-" + nextToken;
         }
 
-        Month.valueOf(str.toUpperCase()).getValue();
-        return str;
+
+        return "";
     }
 
+    public boolean isMonth(String targetValue) {
+        targetValue=targetValue.toLowerCase();
+        for(String s: dateArr){
+            if(s.equals(targetValue))
+                return true;
+        }
+        return false;
+    }
 
+    public boolean isYear(String targetValue) {
+        if(targetValue.length()==4) {
+            return true;
+        }
+        return false;
+    }
 }
