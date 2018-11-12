d1e0c0c50cca356a31d2a2e645a98cce1571c822
Nir Kutsky <nirk2012@gmail.com>
2018-11-12 15:04:03 +0200

    starting price class

--- src/main/java/Model/Parse/ANumbers.java
+++ src/main/java/Model/Parse/ANumbers.java
@@ -2,28 +2,18 @@ package Model.Parse;
 
 public class ANumbers {
 
-    private String K;
-    private String M;
-    private String B;
-    final long MILLION = 1000000L;
-    final long BILLION = 1000000000L;
-
-    public ANumbers() {
-        K = "K";
-        M = "M";
-        B = "B";
-    }
-
-    public String classifyNumber(String str){
+    public static String classifyNumber(String str){
+        final long MILLION = 1000000L;
+        final long BILLION = 1000000000L;
         double number;
         number = Double.parseDouble(str);
 
         if ((number >= 1000) && (number < MILLION))
-            return "" + number/1000 + K;
+            return "" + number/1000 + "K";
         if((number >= MILLION) && (number < BILLION))
-            return "" + number/MILLION + M;
+            return "" + number/MILLION + "M";
         if(number >= BILLION)
-            return "" + number/BILLION + B;
+            return "" + number/BILLION + "B";
 
         return str;
     }
