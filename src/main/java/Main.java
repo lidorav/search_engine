f840ffe52e6d9aa7b26ea0290cc023850f797b2a
lidorav <38429030+lidorav@users.noreply.github.com>
2018-11-12 15:03:37 +0200

    Merge branch 'master' of https://github.com/lidorav/search_engine
    # Conflicts:
    #	.idea/vcs.xml
    #	src/main/java/Model/ReadFile.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,6 @@ public class Main {
         System.out.println(temp.classifyNumber("10045400403"));
         */
 
-       
+
     }
 }
--- src/main/java/Model/Parse/Percentage.java
+++ src/main/java/Model/Parse/Percentage.java
@@ -2,7 +2,24 @@ package Model.Parse;
 
 public class Percentage extends ANumbers {
 
+    private final String precent ="%";
 
+
+    public String toPrecent(int index, String token){
+
+        if(token.contains("%"))
+            return token;
+        
+        if(token.contains("precent")) {
+            token = token.replace(" percent", this.precent);
+        }
+
+        if(token.contains("precentage")) {
+            token = token.replace(" precentage", this.precent);
+        }
+        return token;
+
+    }
 
 
 }
