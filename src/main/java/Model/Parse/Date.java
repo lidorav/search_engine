366eb8cc78d8f2c23b29efcc7fa40a3ab02550a4
lidorav <38429030+lidorav@users.noreply.github.com>
2018-11-13 17:54:52 +0200

    Merge branch 'master' of https://github.com/lidorav/search_engine
    # Conflicts:
    #	src/main/java/Main.java

--- src/main/java/Model/Parse/Date.java
+++ src/main/java/Model/Parse/Date.java
@@ -8,41 +8,53 @@ public class Date {
             "may" ,"june" ,"jun" ,"july" ,"jul" ,"august" ,"aug" ,"september" ,"sep" ,
             "october" ,"oct" ,"november" ,"nov" ,"december" ,"dec"};
 
+
     public static String dateParse (int index , String str) {
         String nextToken = Parser.getTokenFromList(index + 1);
 
         //DD Month pattern
         if (str.chars().allMatch(Character::isDigit) && isMonth(nextToken)) {
+            if(isShortMonth(nextToken))
+                nextToken=toLongMonth(nextToken);
             Parser.index++;
             return Month.valueOf(nextToken.toUpperCase()).getValue() + "-" + str;
         }
         //Month DD pattern
         if(isMonth(str) && !isYear(nextToken)){
+            if(isShortMonth(str))
+                str=toLongMonth(str);
             Parser.index++;
             return Month.valueOf(str.toUpperCase()).getValue() + "-" + nextToken;
         }
         //YYYY MM pattern
         if(isMonth(str) && isYear((nextToken))){
+            if(isShortMonth(str))
+                str=toLongMonth(str);
             Parser.index++;
             return Month.valueOf(str.toUpperCase()).getValue() + "-" + nextToken;
         }
-
         return "";
     }
 
     private static boolean isMonth(String targetValue) {
         targetValue=targetValue.toLowerCase();
-        if(targetValue.length() == 3 && !targetValue.equals("may"))
-            targetValue = shortMonth(targetValue);
-
         for(String s: dateArr){
             if(s.equals(targetValue))
-                return true;
+            return true;
         }
         return false;
     }
 
-    private static String shortMonth(String targetValue){
+    private static boolean isShortMonth(String token){
+        token=token.toLowerCase();
+        if(token.length() == 3 && !token.equals("may"))
+            return true;
+
+        return false;
+    }
+
+    private static String toLongMonth(String targetValue){
+        targetValue=targetValue.toLowerCase();
         for (int i=0; i <dateArr.length;i++){
              if(dateArr[i].contains(targetValue) && dateArr[i].length()>3){
                  return dateArr[i];
