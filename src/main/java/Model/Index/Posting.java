68d1c26bed251b64a5c0582fadac898d8100c8a8
lidorav <38429030+lidorav@users.noreply.github.com>
2018-11-24 13:21:18 +0200

    cbcbs

--- src/main/java/Model/Index/Posting.java
+++ src/main/java/Model/Index/Posting.java
@@ -1,4 +1,33 @@
 package Model.Index;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Posting {
+
+    private String path = "C:\\Users\\USER\\Desktop\\retrivel\\WORK\\Posting";
+    public Posting(){
+        new File(path).mkdirs();
+        for(int i=0 ; i<=9 ; i++){
+            String fileName = i + ".txt";
+            try {
+                new File(path+"\\"+fileName).createNewFile();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        for(char alphabet = 'a'; alphabet <='z'; alphabet++ ) {
+            String fileName = alphabet + ".txt";
+            try {
+                    new File(path + "\\" + fileName).createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
+
 }
